abdb2d65422e48b60b3c92d0a5a3ed01d1139384
VladimirGorin <vladimirgorin410@gmail.com>
2025-03-12 05:33:07 +0600

    traning

---
+++ base-traning/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("\tHello Tab");
+        // System.out.println("Hello Next\nline");
+        // System.out.println("Hell\"o World!");
+        // System.out.println("Hello \\ Slash!");
+
+
+        // // byte - предназначен для хранения целых чисел. Диапазон чисел от -128 до 127, если число будет большим 127 или меньшим -128 , то выдастся ошибка;
+        // // short - предназначен для хранения целых чисел. Диапазон чисел от -32768 до 32767;
+        // // int - предназначен для хранения целых чисел. Диапазон чисел от -2147483648 до 2147483647;
+        // // long - предназначен для хранения целых чисел. Диапазон чисел от –9 223 372 036 854 775 808 до 9 223 372 036 854 775 807.
+        // // float - для создания чисел с плавающей точкой. Диапазон чисел от от -3.4*1038 до 3.4*1038;
+        // // double - для создания чисел с плавающей точкой. Диапазон чисел от от ±4.9*10-324 до ±1.8*10308.
+
+        // // bool - логический тип данных. Предназначен для записи истинного (true) или ложного (false) значения;
+        // // char - тип данных для работы с символами. Позволяет поместить в одинарных кавычках какой-либо символ;
+        // // String - тип данных для текста. В двойных кавычках можно сохранить одно или множество слов.
+
+        // byte somebyte = 0;
+        // short someshort = 0;
+        // int someint = 0;
+        // long somelong = 0;
+        // float somefloat = 1.1000f;
+        // double somedouble = 1.1002d;
+
+
+        // char somechar = '%';
+        // String somestring = "asd";
+        // boolean someboolean = true;
+
+
+        Scanner scan = new Scanner(System.in);
+
+        float someNumber1, someNumber2, res;
+
+        System.out.print("Введите число 1: ");
+        someNumber1 = scan.nextFloat();
+
+        System.out.print("Введите число 2: ");
+        someNumber2 = scan.nextFloat();
+
+        res = someNumber1 + someNumber2;
+        System.out.println("Ваш результат: " + res);
+
+        // System.out.println("Введите ваше имя: ");
+        // String username = scan.nextLine();
+        // System.out.println("Введите ваш возраст: ");
+        // int userAge = scan.nextInt();
+
+        // System.out.println(userAge);
+        // System.out.println(username);
+
+        scan.close();
+    }
+}
